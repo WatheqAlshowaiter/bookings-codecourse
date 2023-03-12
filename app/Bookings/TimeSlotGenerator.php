ea24231d2bbe570695fa37ea3151d5a0e7f1fa4c
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2023-03-12 22:47:37 +0300

    complete #6

--- app/Bookings/TimeSlotGenerator.php
+++ app/Bookings/TimeSlotGenerator.php
@@ -3,20 +3,41 @@
 namespace App\Bookings;
 
 use App\Models\Schedule;
+use App\Models\Service;
 use Carbon\CarbonInterval;
 
 class TimeSlotGenerator
 {
     public const INCREMENT = 15;
+
+    public $schedule;
+
     protected $interval;
 
-    public function __construct(Schedule $schedule)
+    public function __construct(Schedule $schedule, Service $service)
     {
-        $this->interval =  CarbonInterval::minutes(self::INCREMENT)
+        $this->schedule = $schedule;
+
+        $this->interval = CarbonInterval::minutes(self::INCREMENT)
             ->toPeriod(
                 $schedule->date->setTimeFrom($schedule->start_time),
-                $schedule->date->setTimeFrom($schedule->end_time),
+                $schedule->date->setTimeFrom(
+                    $schedule->end_time->subMinutes($service->duration)
+                ),
             );
+    }
+
+    public function applyFilters(array $filters)
+    {
+        foreach ($filters as $filter) {
+            if (! $filter instanceof Filter) {
+                continue;
+            }
+
+            $filter->apply($this, $this->interval);
+        }
+
+        return $this;
     }
 
     public function get()
--- app/Http/Controllers/BookingsController.php
+++ app/Http/Controllers/BookingsController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Bookings\Filters\SlotsPassedTodayFilter;
 use App\Bookings\TimeSlotGenerator;
 use App\Models\Schedule;
-use Carbon\CarbonInterval;
+use App\Models\Service;
 use Illuminate\Http\Request;
 
 class BookingsController extends Controller
@@ -14,13 +15,17 @@ class BookingsController extends Controller
      */
     public function __invoke(Request $request)
     {
-
         $schedule = Schedule::find(1);
+        $service = Service::find(1);
 
-        $slots = (new TimeSlotGenerator($schedule))->get();
+        $slots = (new TimeSlotGenerator($schedule, $service))
+            ->applyFilters([
+                new SlotsPassedTodayFilter(),
+            ])
+            ->get();
 
         return view('bookings.create', [
-            'slots' => $slots
+            'slots' => $slots,
         ]);
     }
 }
