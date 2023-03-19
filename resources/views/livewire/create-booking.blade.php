df1d1db33c4617cf3fd88dc518a3b4396f1dc141
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2023-03-20 01:33:33 +0300

    complete #15

--- resources/views/livewire/create-booking.blade.php
+++ resources/views/livewire/create-booking.blade.php
@@ -11,15 +11,24 @@
             </select>
         </div>
 
-        <div class="mb-6 {{!$employees->count() ? 'opacity-25' : ''}}">
+        <div class="mb-6 {{ !$employees->count() ? 'opacity-25' : '' }}">
             <label for="employee" class="inline-block text-gray-700 font-bold mb-2">Select employee</label>
             <select name="employee" id="service" class="bg-white h-10 w-full border-none rounded-lg"
-                wire:model="state.employee" {{!$employees->count() ? 'disabled' : ''}}>
+                wire:model="state.employee" {{ !$employees->count() ? 'disabled' : '' }}>
                 <option value="">Select an employee</option>
                 @foreach ($employees as $employee)
                     <option value="{{ $employee->id }}">{{ $employee->name }}</option>
                 @endforeach
             </select>
         </div>
+
+        <div class="mb-6 {{ !$this->selectedService || !$this->selectedEmployee ? 'opacity-25' : '' }}">
+            <label for="appointment" class="inline-block text-gray-700 font-bold mb-2">Select appointment</label>
+        </div>
+
+
+
+
+
     </form>
 </div>
